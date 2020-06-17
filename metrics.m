0f014cbbb9d42c9209da085ed1928957a5b714fb
Radu Serban <serban@wisc.edu>
2020-06-17 14:51:20 +0200

    Update metrics.m function to also report speed deviation metrix relative to leader's speed

--- metrics.m
+++ metrics.m
@@ -1,8 +1,8 @@
-function [mPath, mSpeed, path_length] = metrics(lfile, ffile, varargin)
+function [mPath, mSpeed, mSpeedRel, path_length] = metrics(lfile, ffile, varargin)
 % Calculate metrics for follower path deviation.
 % Usage:
-%   [avg_dev, max_dev, path_length] = metrics(lfile, ffile)
-%   [avg_dev, max_dev, path_length] = metrics(lfile, ffile, plotit)
+%   [mPath, mSpeed, mSpeedRel, path_length] = metrics(lfile, ffile)
+%   [mPath, mSpeed, mSpeedRel, path_length] = metrics(lfile, ffile, plotit)
 % Inputs:
 %   lfile - filename (csv) with leader's path data
 %   ffile - filename (csv) with follower's path data
@@ -106,10 +106,31 @@ fspeedS(k) = fspeedC(fidx);
 end
 time_speed = toc(tstart_speed);
 
-% Calculate speed metrics
+% Note: for speed metric calculations, discard an initial segment of the
+% common path (always a fixed value).  This is because:
+% - experiments atsrt with all vehicles at rest
+% - they all need to accelerate from rest to reach the "convoy speed"
+% - at t=0, the relative speed deviation would involve a division by 0
+% Ideally, the test setup would be modified to collect data between to
+% pre-defined "start" and "end" points on the prescribed path with all
+% followers using PID controllers before reaching the "start" point and
+% then switching to an inference controller.
+buf_length = 10;  % meters
+is = round(buf_length / delta);
+ns = n - is + 1;
+fspeedS = fspeedS(is:end);
+lspeedS = lspeedS(is:end);
+
+
+% Calculate speed metrics (absolute speed deviation)
 speed_deviation = abs(fspeedS - lspeedS);
-mSpeed.avg = sum(speed_deviation) / n;
+mSpeed.avg = sum(speed_deviation) / ns;
 mSpeed.max = max(speed_deviation);
+
+% Calculate speed metrics (absolute speed deviation)
+speed_deviation_rel = speed_deviation ./ abs(lspeedS);
+mSpeedRel.avg = sum(speed_deviation_rel) / ns;
+mSpeedRel.max = max(speed_deviation_rel);
 
 clear lidx fidx
 
@@ -175,9 +196,9 @@ if plotit
     
     % Figure 3: Vehicle speeds at same locations along leader path
     figure
-    plot((1:n)*delta, lspeedS, 'color', lcol)
+    plot((is:n)*delta, lspeedS, 'color', lcol)
     hold on, grid on, box on
-    plot((1:n)*delta, fspeedS, 'color', fcol)
+    plot((is:n)*delta, fspeedS, 'color', fcol)
     xlabel('Distance along leader path [m]')
     ylabel('Vehicle speed [m/s]')
     title('Vehicle speeds at same locations')
@@ -185,10 +206,18 @@ if plotit
     
     % Figure 4: Speed deviation as function of distance traveled along leader path
     figure
-    plot((1:n)*delta, speed_deviation)
+    plot((is:n)*delta, speed_deviation)
     grid on, box on
     xlabel('Distance along leader path [m]')
     ylabel('Follower speed deviation [m/s]')
     title('Speed deviation')
-   
+
+    % Figure 5: Speed deviation as function of distance traveled along leader path
+    figure
+    plot((is:n)*delta, speed_deviation_rel)
+    grid on, box on
+    xlabel('Distance along leader path [m]')
+    ylabel('Follower speed deviation [-]')
+    title('Relative speed deviation')
+
 end
